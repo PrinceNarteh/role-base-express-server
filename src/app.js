6a0454722e4cf8a2bdb6cb16a30e0fc9a6c17799
Prince Narteh <prinarteh@gmail.com>
2022-02-26 15:31:17 +0000

    add catch all error

--- src/app.js
+++ src/app.js
@@ -6,4 +6,10 @@ app.get('/health-check', (req, res) => {
   res.send('Server up and running');
 });
 
+app.get('/*', (req, res) => {
+  res.status(404).json({ message: 'Page not found' });
+});
+
+app.use((err, req, res) => {});
+
 module.exports = app;
