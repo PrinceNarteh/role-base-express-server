941474535f5505d7ceec5af2ee67bb571f742ab1
Prince Narteh <prinarteh@gmail.com>
2022-03-01 21:40:26 +0000

    fix cors error

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ app.use(morgan('dev'));
 const whitelist = ['http://localhost:3000'];
 const corsOptions: CorsOptions = {
   origin: (origin, callback) => {
-    if (origin && whitelist.indexOf(origin) !== -1) {
+    if (!origin || (origin && whitelist.indexOf(origin) !== -1)) {
       callback(null, true);
     } else {
       callback(new AppError('Not allowed by CORS', 400));
--- src/helpers/validation.ts
+++ src/helpers/validation.ts
@@ -1,4 +1,4 @@
-import z from 'zod';
+import { z } from 'zod';
 
 export const registerValidator = z.object({
   username: z.string().min(3),
