18853395e8e2648d94dfe1187273ba8cadf27096
Prince Narteh <prinarteh@gmail.com>
2022-03-06 14:27:22 +0000

    adds getUser handler

--- src/routes/users/users.controller.ts
+++ src/routes/users/users.controller.ts
@@ -1,8 +1,18 @@
 import { Request, Response } from 'express';
 import User from '../../models/user.model';
+import AppError from '../../utils/appError';
 import { asyncHandler } from '../../utils/asyncHandler';
 
 export const getAllUsers = asyncHandler(async (req: Request, res: Response) => {
   const users = await User.find({});
   res.status(200).json({ status: 'success', users });
+});
+
+export const getUser = asyncHandler(async (req: Request, res: Response) => {
+  const userId = req.params;
+  const user = await User.findOne({ _id: userId });
+  if (!user) {
+    throw new AppError(`User with ID ${userId} not found`, 404);
+  }
+  res.status(200).json({ status: 'success', user });
 });
--- src/routes/users/users.router.ts
+++ src/routes/users/users.router.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { verifyJWT } from '../../middleware/verifyJWT';
 import { ROLES_LIST, verifyRoles } from '../../middleware/verifyRoles';
-import { getAllUsers } from './users.controller';
+import { getAllUsers, getUser } from './users.controller';
 
 const usersRouter = Router();
 
 usersRouter.get('/', verifyJWT, verifyRoles(ROLES_LIST.Admin), getAllUsers);
+usersRouter.get('/:userId', verifyJWT, verifyRoles(ROLES_LIST.Admin), getUser);
 
 export default usersRouter;
